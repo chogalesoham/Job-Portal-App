edf9746bfd038525d2e4aaad63e96fc6d7bf93a8
soham chogale <chogalesoham74@gmail.com>
2024-11-17 22:21:39 +0530

    feat/initialize React app with Tailwind CSS and ShadCN Ui setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+const App = () => {
+  return (
+    <div>
+      <h1>App</h1>
+      <Button>click mi</Button>
+    </div>
+  );
+};
+
+export default App;
