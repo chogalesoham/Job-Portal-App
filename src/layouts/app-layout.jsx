56142ffe9abdf22f4a9992c23bf846c53cf48e70
soham chogale <chogalesoham74@gmail.com>
2024-11-18 17:19:07 +0530

    feat/setup supabese and clerk authontication

--- src/layouts/app-layout.jsx
+++ src/layouts/app-layout.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import "../App.css";
+import Header from "@/components/header";
 
 const AppLayout = () => {
   return (
     <div>
-      <h1>AppLayout</h1>
-      <Outlet />
+      <div className="grid-background"></div>
+      <main className=" min-h-screen container mx-auto px-6">
+        <Header />
+        <Outlet />
+      </main>
+      <footer className=" p-10 text-center bg-gray-800 mt-10">
+        Made With 💗 By Soham Chogale
+      </footer>
     </div>
   );
 };
