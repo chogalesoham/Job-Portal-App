3690b664a4d756038fb7ad814426f530a6e00f06
soham chogale <chogalesoham74@gmail.com>
2024-12-07 16:12:56 +0530

    feat/add apply jobs api integration

---
+++ src/api/apiApplication.js
@@ -0,0 +1,34 @@
+import supabaseClient from "@/utils/supabase";
+import { supabaseUrl } from "@/utils/supabase";
+
+export async function applyToJob(token, _, jobData) {
+  const supabase = await supabaseClient(token);
+
+  const random = Math.floor(Math.random() * 90000);
+  const fileName = `resume-${random}-${jobData.candidate_id}`;
+  const { error: storageError } = await supabase.storage
+    .from("resumes")
+    .upload(fileName, jobData.resume);
+
+  if (storageError) {
+    console.error("Error Uploading Resume Companies: ", storageError);
+    return null;
+  }
+  const resume = `${supabaseUrl}/storage/v1/object/public/resumes/${fileName}`;
+
+  const { data, error } = await supabase
+    .from("applications")
+    .insert([
+      {
+        ...jobData,
+        resume,
+      },
+    ])
+    .select();
+
+  if (error) {
+    console.error("Error Submitting Application: ", error);
+    return null;
+  }
+  return data;
+}
